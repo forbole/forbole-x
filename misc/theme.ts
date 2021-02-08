579c3cfec4703b0ed5be04d758d7e4cc028ef3b2
Calvin Kei <calvin.conceptz@gmail.com>
2021-02-08 18:00:26 +0800

    update theme shadow

--- misc/theme.ts
+++ misc/theme.ts
@@ -61,7 +61,7 @@ const common = {
       textTransform: 'none',
     },
   },
-  shadows: Array(25).fill('none'),
+  shadows: [...Array(7).fill('none'), ...Array(18).fill('0px 3px 16px #00000029')],
 }
 
 const lightTheme = {
