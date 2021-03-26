abf2326841e83692cc0a1fa421479d001ce1bc39
Calvin Kei <calvin.conceptz@gmail.com>
2021-03-26 18:10:59 +0800

    remove commented lines

--- components/ActivitiesTable/styles.ts
+++ components/ActivitiesTable/styles.ts
@@ -34,7 +34,6 @@ export const useGetStyles = (tag?: string) => {
       },
       rowContainer: {
         display: 'flex',
-        // alignItems: 'center',
       },
       tagContainer: {
         width: theme.spacing(24),
@@ -82,36 +81,3 @@ export const useGetStyles = (tag?: string) => {
     classes: useStyles(),
   }
 }
-
-// const useStyles = makeStyles((theme) => ({
-//   rowHeader: {
-//     background: theme.palette.grey[400],
-//     padding: theme.spacing(0, 2),
-//     color: theme.palette.grey[500],
-//     display: 'flex',
-//     alignItems: 'center',
-//     justifyContent: 'space-between',
-//   },
-//   row: {
-//     padding: theme.spacing(3.25, 2),
-//   },
-//   table: {
-//     border: `1px solid ${theme.palette.grey[100]}`,
-//     borderRadius: theme.shape.borderRadius,
-//   },
-//   checkIcon: {
-//     width: theme.spacing(2),
-//     height: theme.spacing(2),
-//     verticalAlign: 'middle',
-//     marginLeft: theme.spacing(0.5),
-//     color: theme.palette.success.main,
-//   },
-//   tabIndicator: {
-//     backgroundColor: theme.palette.text.primary,
-//   },
-//   typograph: {
-//     padding: theme.spacing(2, 0),
-//   },
-// }))
-
-// export default useStyles
