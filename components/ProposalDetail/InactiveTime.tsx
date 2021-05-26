a55dbf449f06904cfa4214d92f49a5c813318af7
YuhsinLiao1995 <61954949+YuhsinLiao1995@users.noreply.github.com>
2021-05-26 19:15:04 +0800

    fix

--- components/ProposalDetail/InactiveTime.tsx
+++ components/ProposalDetail/InactiveTime.tsx
@@ -1,7 +1,6 @@
 import { Box, Typography } from '@material-ui/core'
 import useTranslation from 'next-translate/useTranslation'
 import React from 'react'
-import { Proposal } from './index'
 
 interface ActiveProps {
   proposal: Proposal
