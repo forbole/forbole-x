fa3660739ffce916b5020db39df8b65852f73832
Calvin Kei <calvin.conceptz@gmail.com>
2021-06-21 22:18:48 +0800

    update useSignerInfo method

--- misc/useSignerInfo.ts
+++ misc/useSignerInfo.ts
@@ -1,21 +1,23 @@
 import React from 'react'
 import { SigningStargateClient } from '@cosmjs/stargate'
-import sendMsgToChromeExt from './sendMsgToChromeExt'
 import cryptocurrencies from './cryptocurrencies'
-
-const getSequenceAndChainId = async (address: string, crypto: string): Promise<any> => {
-  const client = await SigningStargateClient.connect(cryptocurrencies[crypto].rpcEndpoint)
-  const { accountNumber, sequence } = await client.getSequence(address)
-  const chainId = await client.getChainId()
-  return { accountNumber, sequence, chainId }
-}
 
 const useSignerInfo = (account: Account) => {
   const [signerInfo, setSignerInfo] = React.useState({})
 
+  const getSequenceAndChainId = React.useCallback(
+    async (address: string, crypto: string): Promise<any> => {
+      const client = await SigningStargateClient.connect(cryptocurrencies[crypto].rpcEndpoint)
+      const { accountNumber, sequence } = await client.getSequence(address)
+      const chainId = await client.getChainId()
+      setSignerInfo({ accountNumber, sequence, chainId })
+    },
+    []
+  )
+
   React.useEffect(() => {
     if (account) {
-      getSequenceAndChainId(account.address, account.crypto).then((result) => setSignerInfo(result))
+      getSequenceAndChainId(account.address, account.crypto)
     }
   }, [account])
 
