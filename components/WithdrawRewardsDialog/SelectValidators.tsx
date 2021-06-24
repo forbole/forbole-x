740a6d56024d2372cf2d407699c09a08420b579f
YuhsinLiao1995 <61954949+YuhsinLiao1995@users.noreply.github.com>
2021-06-24 17:07:41 +0800

    update withdraw reward dialog

--- components/WithdrawRewardsDialog/SelectValidators.tsx
+++ components/WithdrawRewardsDialog/SelectValidators.tsx
@@ -22,6 +22,8 @@ import { formatCurrency, formatTokenAmount, getTokenAmountBalance } from '../../
 import useStyles from './styles'
 import { ValidatorTag } from './index'
 import ValidatorAvatar from '../ValidatorAvatar'
+import ImageDefaultDark from '../../assets/images/image_default_dark.svg'
+import ImageDefaultLight from '../../assets/images/image_default_light.svg'
 
 interface SelectValidatorsProps extends Partial<FilledTextFieldProps> {
   onConfirm(delegations: ValidatorTag[], m: string): void
@@ -42,14 +44,14 @@ const SelectValidators: React.FC<SelectValidatorsProps> = ({
 }) => {
   const { t, lang } = useTranslation('common')
   const classes = useStyles()
-  const { currency } = useGeneralContext()
+  const { currency, theme } = useGeneralContext()
   const [amount, setAmount] = React.useState<TokenAmount>({})
   const [page, setPage] = React.useState(0)
   const [value, setValue] = React.useState('')
   const [rowsPerPage, setRowsPerPage] = React.useState(10)
   // const [currentTab, setCurrentTab] = React.useState(0)
   const [state, setState] = React.useState({})
-  const theme = useTheme()
+  const themeStyle = useTheme()
 
   const handleChange = (event) => {
     setState({ ...state, [event.target.name]: event.target.checked })
@@ -94,6 +96,7 @@ const SelectValidators: React.FC<SelectValidatorsProps> = ({
     })
     return total
   }, [validatorList])
+  console.log('totalAmount', totalAmount)
 
   const onSelect = (address) => {
     const index = validatorList.findIndex((v) => v.address === address)
@@ -134,119 +137,149 @@ const SelectValidators: React.FC<SelectValidatorsProps> = ({
   }, [])
 
   return (
-    <form
-      noValidate
-      onSubmit={(e) => {
-        e.preventDefault()
-        onConfirm(
-          validatorList.filter((v) => v.isSelected === true),
-          value
-        )
-      }}
-    >
-      <DialogContent className={classes.dialogContent}>
-        <Box mb={10}>
-          <Box mt={4} mb={2}>
-            <Typography className={classes.totalReward}>
-              {t('total reward amount')}&nbsp;
-              {formatTokenAmount(totalAmount, account.crypto, lang, ', ')}
-            </Typography>
-            <Typography>{t('withdraw amount')}</Typography>
-            <Typography variant="h1" className={classes.h1}>
-              {formatTokenAmount(amount, account.crypto, lang, ', ')}
-            </Typography>
-            <Typography>
-              {t('select the validator below you want to claim the reward amount')}
-            </Typography>
-          </Box>
-          <FormControlLabel
-            value="end"
-            control={<Checkbox onClick={handleSelectAll} color="primary" size="small" />}
-            label={<Typography>{t('withdraw all')}</Typography>}
-            labelPlacement="end"
-          />
-          <Box mt={0}>
-            {validatorList.slice(page * rowsPerPage, (page + 1) * rowsPerPage).map((v) => (
+    <>
+      {totalAmount[account.crypto] ? (
+        <form
+          noValidate
+          onSubmit={(e) => {
+            e.preventDefault()
+            onConfirm(
+              validatorList.filter((v) => v.isSelected === true),
+              value
+            )
+          }}
+        >
+          <DialogContent className={classes.dialogContent}>
+            <Box mb={10}>
+              <Box mt={4} mb={2}>
+                <Typography className={classes.totalReward}>
+                  {t('total reward amount')}&nbsp;
+                  {formatTokenAmount(totalAmount, account.crypto, lang, ', ')}
+                </Typography>
+                <Typography>{t('withdraw amount')}</Typography>
+                <Typography variant="h1" className={classes.h1}>
+                  {formatTokenAmount(amount, account.crypto, lang, ', ')}
+                </Typography>
+                <Typography>
+                  {t('select the validator below you want to claim the reward amount')}
+                </Typography>
+              </Box>
               <FormControlLabel
-                key={v.address}
-                className={classes.controllLabel}
                 value="end"
-                control={
-                  <Checkbox
-                    checked={v.isSelected}
-                    onClick={() => onSelect(v.address)}
-                    onChange={handleChange}
-                    color="primary"
-                    size="small"
-                    name={v.address}
-                  />
-                }
-                label={
-                  <Box display="flex" alignItems="center" style={{ flex: 1, width: '25rem' }}>
-                    <ValidatorAvatar crypto={crypto} withoutLink validator={v} size="small" />
-                    <Typography className={classes.rewardsAmount}>
-                      {formatTokenAmount(v.rewards, account.crypto, lang, ', ')}
-                    </Typography>
-                  </Box>
-                }
+                control={<Checkbox onClick={handleSelectAll} color="primary" size="small" />}
+                label={<Typography>{t('withdraw all')}</Typography>}
                 labelPlacement="end"
               />
-            ))}
-          </Box>
-          <TablePagination
-            className={classes.tablePagination}
-            hideFooter
-            page={page}
-            rowsPerPage={rowsPerPage}
-            rowsCount={validators.length}
-            onPageChange={setPage}
-            onRowsPerPageChange={setRowsPerPage}
-          />
-        </Box>
-        <Typography>{t('memo')}</Typography>
-        <TextField
-          InputProps={{
-            disableUnderline: true,
-          }}
-          fullWidth
-          variant="filled"
-          value={value}
-          placeholder={t('description optional')}
-          multiline
-          rows={4}
-          onChange={(e) => setValue(e.target.value)}
-        />
-      </DialogContent>
-      <DialogActions>
-        <Box
-          flex={1}
-          display="flex"
-          justifyContent="space-between"
-          alignItems="center"
-          mb={3}
-          mx={2}
-        >
-          <Box>
-            <Typography variant="h5">{formatTokenAmount(amount, account.crypto, lang)}</Typography>
-            <Typography>{formatCurrency(getTokenAmountBalance(amount), currency, lang)}</Typography>
-          </Box>
-          <Button
-            variant="contained"
-            classes={{ root: classes.button }}
-            color="primary"
-            disabled={
-              loading ||
-              !Object.values(amount)
-                .map((a) => a.amount)
-                .reduce((a, b) => a + b, 0)
-            }
-            type="submit"
-          >
-            {loading ? <CircularProgress size={theme.spacing(3.5)} /> : t('next')}
-          </Button>
-        </Box>
-      </DialogActions>
-    </form>
+              <Box mt={0}>
+                {validatorList.slice(page * rowsPerPage, (page + 1) * rowsPerPage).map((v) => (
+                  <FormControlLabel
+                    key={v.address}
+                    className={classes.controllLabel}
+                    value="end"
+                    control={
+                      <Checkbox
+                        checked={v.isSelected}
+                        onClick={() => onSelect(v.address)}
+                        onChange={handleChange}
+                        color="primary"
+                        size="small"
+                        name={v.address}
+                      />
+                    }
+                    label={
+                      <Box display="flex" alignItems="center" style={{ flex: 1, width: '25rem' }}>
+                        <ValidatorAvatar crypto={crypto} withoutLink validator={v} size="small" />
+                        <Typography className={classes.rewardsAmount}>
+                          {formatTokenAmount(v.rewards, account.crypto, lang, ', ')}
+                        </Typography>
+                      </Box>
+                    }
+                    labelPlacement="end"
+                  />
+                ))}
+              </Box>
+              <TablePagination
+                className={classes.tablePagination}
+                hideFooter
+                page={page}
+                rowsPerPage={rowsPerPage}
+                rowsCount={validators.length}
+                onPageChange={setPage}
+                onRowsPerPageChange={setRowsPerPage}
+              />
+            </Box>
+            <Typography>{t('memo')}</Typography>
+            <TextField
+              InputProps={{
+                disableUnderline: true,
+              }}
+              fullWidth
+              variant="filled"
+              value={value}
+              placeholder={t('description optional')}
+              multiline
+              rows={4}
+              onChange={(e) => setValue(e.target.value)}
+            />
+          </DialogContent>
+          <DialogActions>
+            <Box
+              flex={1}
+              display="flex"
+              justifyContent="space-between"
+              alignItems="center"
+              mb={3}
+              mx={2}
+            >
+              <Box>
+                <Typography variant="h5">
+                  {formatTokenAmount(amount, account.crypto, lang)}
+                </Typography>
+                <Typography>
+                  {formatCurrency(getTokenAmountBalance(amount), currency, lang)}
+                </Typography>
+              </Box>
+              <Button
+                variant="contained"
+                classes={{ root: classes.button }}
+                color="primary"
+                disabled={
+                  loading ||
+                  !Object.values(amount)
+                    .map((a) => a.amount)
+                    .reduce((a, b) => a + b, 0)
+                }
+                type="submit"
+              >
+                {loading ? <CircularProgress size={themeStyle.spacing(3.5)} /> : t('next')}
+              </Button>
+            </Box>
+          </DialogActions>
+        </form>
+      ) : (
+        <>
+          <DialogContent className={classes.dialogContent}>
+            <Box justifyContent="center" display="flex" mt={6}>
+              {theme === 'light' ? <ImageDefaultLight /> : <ImageDefaultDark />}
+            </Box>
+            <Box textAlign="center" mt={4}>
+              <Typography>{t('no rewards yet')}</Typography>
+              <Typography variant="subtitle2" color="textSecondary">
+                {t('delegate your token and get some rewards')}
+              </Typography>
+            </Box>
+            {/* <ImageDefaultLight /> */}
+          </DialogContent>
+          <DialogActions>
+            <Box justifyContent="center" display="flex" flex={1} mb={6} mt={2}>
+              <Button variant="contained" classes={{ root: classes.button }} color="primary">
+                {t('delegate')}
+              </Button>
+            </Box>
+          </DialogActions>
+        </>
+      )}
+    </>
   )
 }
 
