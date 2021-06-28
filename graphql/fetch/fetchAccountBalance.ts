a4052e45ff59892ae695c6e4e159c637c29c76e7
Calvin Kei <calvin.conceptz@gmail.com>
2021-06-28 17:47:54 +0800

    add locale

--- graphql/fetch/fetchAccountBalance.ts
+++ graphql/fetch/fetchAccountBalance.ts
@@ -2,12 +2,16 @@ import cryptocurrencies from '../../misc/cryptocurrencies'
 import { getTotalTokenAmount, transformGqlAcountBalance } from '../../misc/utils'
 import { getBalance } from '../queries/accountBalances'
 
-const fetchAccountBalance = async (address: string, crypto: string): Promise<TokenAmount> => {
+const fetchAccountBalance = async (
+  address: string,
+  crypto: string,
+  availableBalanceOnly?: boolean
+): Promise<TokenAmount> => {
   const balance = await fetch(cryptocurrencies[crypto].graphqlHttpUrl, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
-      query: getBalance(crypto),
+      query: getBalance(crypto, availableBalanceOnly),
       variables: { address },
     }),
   }).then((r) => r.json())
