cef19021e46c7f16496045f10e564ddedc52ce52
YuhsinLiao1995 <61954949+YuhsinLiao1995@users.noreply.github.com>
2021-07-05 16:43:19 +0800

    fix empty chart for light mode

--- components/AssetDistributionChart/EmptyState.tsx
+++ components/AssetDistributionChart/EmptyState.tsx
@@ -1,21 +1,38 @@
 import React from 'react'
 import { Box, Typography, useTheme } from '@material-ui/core'
 import useTranslation from 'next-translate/useTranslation'
-import ForboleLogo from '../../assets/images/forboleLogo.svg'
+import ForboleLogoDark from '../../assets/images/forboleLogo_dark.svg'
+import ForboleLogoLight from '../../assets/images/forboleLogo_light.svg'
 import useStyles from './styles'
+import { useGeneralContext } from '../../contexts/GeneralContext'
 
 const EmptyState: React.FC = () => {
   const classes = useStyles()
-  const theme = useTheme()
+  const themeStyle = useTheme()
+  const { theme } = useGeneralContext()
   const { t } = useTranslation('common')
   return (
-    <Box display="flex" flexDirection="column" alignItems="center" height={theme.spacing(33.5)}>
-      <ForboleLogo
-        style={{
-          display: 'flex',
-          marginTop: '1rem',
-        }}
-      />
+    <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      height={themeStyle.spacing(33.5)}
+    >
+      {theme === 'light' ? (
+        <ForboleLogoLight
+          style={{
+            display: 'flex',
+            marginTop: '1rem',
+          }}
+        />
+      ) : (
+        <ForboleLogoDark
+          style={{
+            display: 'flex',
+            marginTop: '1rem',
+          }}
+        />
+      )}
       <Typography className={classes.noAsset}>{t('no asset')}</Typography>
     </Box>
   )
