38c842520b13207cca30ba522260a83041c59c56
Calvin Kei <calvin.conceptz@gmail.com>
2021-07-15 00:36:12 +0800

    remove unused files

--- @cosmjs/ledger-amino/build/demo/node.d.ts
+++
@@ -1,9 +0,0 @@
-import { StdSignature } from "@cosmjs/amino";
-import { LedgerSigner } from "../ledgersigner";
-export declare function createSigner(): Promise<LedgerSigner>;
-export declare function getAccounts(signer: LedgerSigner): Promise<ReadonlyArray<{
-    readonly algo: string;
-    readonly address: string;
-    readonly pubkey: string;
-}>>;
-export declare function sign(signer: LedgerSigner, accountNumber: number, fromAddress: string, toAddress: string): Promise<StdSignature>;
