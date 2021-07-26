415736b0e086c4357af78ca030bae122c02788e4
YuhsinLiao1995 <61954949+YuhsinLiao1995@users.noreply.github.com>
2021-07-26 18:11:16 +0800

    help page ui

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box, Button, Typography } from '@material-ui/core'
+import { Box, Typography } from '@material-ui/core'
 import useTranslation from 'next-translate/useTranslation'
 import Layout from '../components/Layout'
 import SettingTable from '../components/SettingTable'
@@ -13,11 +13,6 @@ const Setting: React.FC = () => {
     <Layout passwordRequired>
       <Box display="flex" alignItems="center" mb={2}>
         <Typography variant="h1">{t('settings')}</Typography>
-        <Box justifyContent="flex-end" display="flex" flex="1">
-          <Button onClick={() => setAddAddressOpen(true)} color="primary" variant="outlined">
-            {t('settings')}
-          </Button>
-        </Box>
       </Box>
       <SettingTable />
       <AddAddressDialog open={addAddressOpen} onClose={() => setAddAddressOpen(false)} />
