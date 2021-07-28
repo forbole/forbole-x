cf378a7f6711f5cf831db8fc40e4e06ea390ce07
YuhsinLiao1995 <61954949+YuhsinLiao1995@users.noreply.github.com>
2021-07-28 15:30:42 +0800

    set up follow us ui

--- components/SettingTable/FollowUsTable.tsx
+++ components/SettingTable/FollowUsTable.tsx
@@ -1,8 +1,10 @@
 import { Divider, Box, Typography, useTheme, Paper } from '@material-ui/core'
 import useTranslation from 'next-translate/useTranslation'
 import React from 'react'
-import { TwitterIcon, TelegramIcon, EmailIcon } from 'react-share'
+import { TwitterIcon, TelegramIcon } from 'react-share'
 import BigDipperIcon from '../../assets/images/icons/icon_bigdipper.svg'
+import ForboleIcon from '../../assets/images/icons/icon_forbole.svg'
+import Githubcon from '../../assets/images/icons/icon_github.svg'
 import useStyles from './styles'
 import useIconProps from '../../misc/useIconProps'
 import { CustomTheme } from '../../misc/theme'
@@ -15,7 +17,7 @@ const FollowUsTable: React.FC = () => {
 
   return (
     <Paper>
-      <Box p={themeStyle.spacing(0.1)} pb={themeStyle.spacing(8)}>
+      <Box p={themeStyle.spacing(0.1)} pb={themeStyle.spacing(7)}>
         <a
           href="https://twitter.com/forbole"
           target="_blank"
@@ -66,7 +68,12 @@ const FollowUsTable: React.FC = () => {
           </Box>
         </a>
         <Divider />
-        <a href="mailto:support@forbole.com" className={classes.externalLink}>
+        <a
+          href="https://www.forbole.com/blog"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={classes.externalLink}
+        >
           <Box
             display="flex"
             alignItems="flex-start"
@@ -74,20 +81,15 @@ const FollowUsTable: React.FC = () => {
             p={themeStyle.spacing(0.2)}
           >
             <Box display="flex">
-              <EmailIcon
-                size={24}
-                round
-                iconFillColor={themeStyle.palette.socialMediaIcon.fill}
-                bgStyle={{ fill: themeStyle.palette.socialMediaIcon.background }}
-              />
-              <Typography className={classes.iconText}>{t('email')}</Typography>
+              <ForboleIcon {...iconProps} />
+              <Typography className={classes.iconText}>{t('forbole blog')}</Typography>
             </Box>
-            <Typography>@Bigger Dipper</Typography>
+            <Typography>forbole.com/blog</Typography>
           </Box>
         </a>
         <Divider />
         <a
-          href="https://twitter.com/forbole"
+          href="https://cosmos.bigdipper.live/"
           target="_blank"
           rel="noopener noreferrer"
           className={classes.externalLink}
@@ -100,9 +102,29 @@ const FollowUsTable: React.FC = () => {
           >
             <Box display="flex">
               <BigDipperIcon {...iconProps} />
-              <Typography className={classes.iconText}>Big Dipper</Typography>
+              <Typography className={classes.iconText}>Big Dipper block explorer</Typography>
             </Box>
             <Typography>bigdipper.live</Typography>
+          </Box>
+        </a>
+        <Divider />
+        <a
+          href="https://github.com/forbole"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={classes.externalLink}
+        >
+          <Box
+            display="flex"
+            alignItems="flex-start"
+            justifyContent="space-between"
+            p={themeStyle.spacing(0.2)}
+          >
+            <Box display="flex">
+              <Githubcon {...iconProps} />
+              <Typography className={classes.iconText}>Github</Typography>
+            </Box>
+            <Typography>github.com/forbole</Typography>
           </Box>
         </a>
         <Divider />
