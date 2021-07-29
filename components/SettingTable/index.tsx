945dac001953c583cfa9f5eb3c7d133142981a9c
YuhsinLiao1995 <61954949+YuhsinLiao1995@users.noreply.github.com>
2021-07-29 14:11:26 +0800

    complete ui

--- components/SettingTable/index.tsx
+++ components/SettingTable/index.tsx
@@ -5,6 +5,7 @@ import useStyles from './styles'
 import GeneralTable from './GeneralTable'
 import HelpTable from './HelpTable'
 import FeedbackTable from './FeedbackTable'
+import FollowUsTable from './FollowUsTable'
 
 export type FavAddress = {
   address: string
@@ -42,6 +43,7 @@ const SettingTable: React.FC = () => {
       {currentTab === 0 ? <GeneralTable /> : null}
       {currentTab === 1 ? <HelpTable /> : null}
       {currentTab === 2 ? <FeedbackTable /> : null}
+      {currentTab === 3 ? <FollowUsTable /> : null}
     </>
   )
 }
