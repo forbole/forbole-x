0daf76ebbab75eb3948b9355cb63d40f64ce0577
Rachel Ho <rachelhoyuenyi@gmail.com>
2021-09-08 16:56:56 +0800

    getValidatorCondition && getValidatorConditionClass utils

--- misc/utils.ts
+++ misc/utils.ts
@@ -652,3 +652,20 @@ export const closeAllLedgerConnections = async () => {
   const devices = await TransportWebHID.list()
   await Promise.all(devices.map((d) => d.close()))
 }
+
+export const getValidatorCondition = (signedBlockWindow: number, missedBlockCounter: number) => {
+  return (1 - (missedBlockCounter / signedBlockWindow)) * 100;
+};
+
+export const getValidatorConditionClass = (condition: number) => {
+  let conditionClass = '';
+  if (condition > 90) {
+    conditionClass = 'green';
+  } else if (condition > 70 && condition < 90) {
+    conditionClass = 'yellow';
+  } else {
+    conditionClass = 'red';
+  }
+
+  return conditionClass;
+};
