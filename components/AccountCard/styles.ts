d2494519f290051e56372fe5b7f8a24cdf01ba66
Rachel Ho <rachelhoyuenyi@gmail.com>
2021-09-27 16:40:47 +0800

    test

--- components/AccountCard/styles.ts
+++ components/AccountCard/styles.ts
@@ -1,7 +1,8 @@
 import { makeStyles } from '@material-ui/core/styles'
+import { CustomTheme } from '../../misc/theme'
 
 const useStyles = makeStyles(
-  (theme) => ({
+  (theme: CustomTheme) => ({
     container: {
       padding: theme.spacing(2),
       cursor: 'pointer',
