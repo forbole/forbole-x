f51b1614dfae0334f87ad63af58adc67fc460e0d
rachelhox <rachelhoyuenyi@gmail.com>
2021-12-14 01:09:55 +0800

    active validators accurate status on delegate page

--- components/Condition/index.tsx
+++ components/Condition/index.tsx
@@ -1,12 +1,30 @@
 import React from 'react'
+import { Box, Button } from '@material-ui/core'
+import useTranslation from 'next-translate/useTranslation'
 import { useStyles } from './styles'
 
 const Condition: React.FC<{
   className?: string
-}> = ({ className }) => {
-  const classes = useStyles()
-
-  return <div className={`${className} ${classes.root}`} />
+  onClick?: () => void
+}> = ({ className, onClick }) => {
+  const classes = useStyles(className)
+  const { t } = useTranslation('common')
+  return onClick ? (
+    <Box
+      display="flex"
+      flexDirection="row"
+      alignItems="center"
+      justifyContent="space-between"
+      onClick={onClick}
+    >
+      <Box className={`${className} ${classes.root}`}> </Box>
+      <Button size="small" color="primary" variant="contained">
+        {t('delegate')}
+      </Button>
+    </Box>
+  ) : (
+    <div className={`${className} ${classes.root}`} />
+  )
 }
 
 export default Condition
