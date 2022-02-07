95669915451792639ec0f9e64957eb87a411d931
Calvin Kei <calvin.conceptz@gmail.com>
2022-02-07 22:19:53 +0800

    add voted UI

--- pages/proposals/[crypto]/[id].tsx
+++ pages/proposals/[crypto]/[id].tsx
@@ -1,10 +1,11 @@
-import { Breadcrumbs, Link as MLink, Typography } from '@material-ui/core'
+import { Box, Breadcrumbs, Link as MLink, Typography, useTheme } from '@material-ui/core'
 import useTranslation from 'next-translate/useTranslation'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { gql, useQuery } from '@apollo/client'
 import React from 'react'
 import get from 'lodash/get'
+import keyBy from 'lodash/keyBy'
 import Layout from '../../../components/Layout'
 import cryptocurrencies from '../../../misc/cryptocurrencies'
 import ProposalDetail from '../../../components/ProposalDetail'
@@ -17,12 +18,16 @@ import {
 import { transformProposal, transformVoteSummary, transformVoteDetail } from '../../../misc/utils'
 import { getTokensPrices } from '../../../graphql/queries/tokensPrices'
 import useLatestAccountBalance from '../../../graphql/hooks/useLatestAccountBalance'
+import { useWalletsContext } from '../../../contexts/WalletsContext'
+import { CustomTheme } from '../../../misc/theme'
 
 const Proposal: React.FC = () => {
   const router = useRouter()
+  const theme: CustomTheme = useTheme()
   const { t } = useTranslation('common')
   const { id, crypto: cryptoId } = router.query
   const crypto = cryptocurrencies[String(cryptoId)]
+  const { accounts } = useWalletsContext()
   const { data: proposalData } = useQuery(
     gql`
       ${getProposal(crypto.name)}
@@ -68,6 +73,16 @@ const Proposal: React.FC = () => {
   const proposal = transformProposal(proposalData, balanceData, depositParamsData)
   const voteSummary = transformVoteSummary(proporslReaultData)
   const voteDetail = transformVoteDetail(voteDetailData, proposal)
+  const voteDetailMap = keyBy(voteDetail, 'voter.address')
+
+  const myVotes = React.useMemo(
+    () =>
+      // accounts
+      //   .filter((a) => !!voteDetailMap[a.address])
+      //   .map((a) => ({ account: a, vote: voteDetailMap[a.address] })),
+      accounts[0] && voteDetail[0] ? [{ account: accounts[0], vote: voteDetail[0] }] : [],
+    [voteDetailMap, accounts]
+  )
 
   return (
     <Layout
@@ -89,6 +104,33 @@ const Proposal: React.FC = () => {
         voteDetails={voteDetail}
         denoms={denoms}
       />
+      {myVotes.length > 0 ? (
+        <Box
+          position="fixed"
+          left={0}
+          right={0}
+          bottom={0}
+          p={1.5}
+          bgcolor={theme.palette.bannerBackground}
+          zIndex={1000}
+          textAlign="center"
+        >
+          {myVotes.map((v) => (
+            <Typography key={v.account.address}>
+              <div
+                style={{ color: theme.palette.common.white }}
+                dangerouslySetInnerHTML={{
+                  __html: t('account voted', {
+                    name: v.account.name,
+                    address: v.account.address,
+                    vote: t(v.vote.answer),
+                  }),
+                }}
+              />
+            </Typography>
+          ))}
+        </Box>
+      ) : null}
     </Layout>
   )
 }
