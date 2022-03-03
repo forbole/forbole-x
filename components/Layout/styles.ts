77e4de8aa962d3171c717001a75b5767bcbce49f
rachelhox <rachelhoyuenyi@gmail.com>
2022-03-03 12:13:25 +0800

    clean up

--- components/Layout/styles.ts
+++ components/Layout/styles.ts
@@ -1,4 +1,4 @@
-import { makeStyles, } from '@material-ui/core/styles'
+import { makeStyles } from '@material-ui/core/styles'
 import { CustomTheme } from '../../misc/theme'
 
 interface StyleProps {
