9c5e063a24d0ae02093e788250368ecea4434a0e
Kevin Pang <moistbobo@gmail.com>
2022-05-21 22:34:13 +0800

    fix: Portfolio graphs

--- graphql/queries/blocks.ts
+++ graphql/queries/blocks.ts
@@ -1,8 +1,17 @@
 export const getBlockByTimestamp = (crypto: string) => `
-  query Block($timestamp: timestamp!) @api(name: ${crypto}bdjuno) {
+  query Block($timestamp: timestamp!){
     block(limit: 1, order_by: { timestamp: desc }, where: { timestamp: { _lte: $timestamp } }) {
       height
       timestamp
     }
   }
 `
+
+// export const getBlockByTimestamp = (crypto: string) => `
+//   query Block($timestamp: timestamp!) @api(name: ${crypto}bdjuno) {
+//     block(limit: 1, order_by: { timestamp: desc }, where: { timestamp: { _lte: $timestamp } }) {
+//       height
+//       timestamp
+//     }
+//   }
+// `
